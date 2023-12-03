a9a1bccebc6b77bd9b82ed704b904d5807e4abbb
TimMark <noierrdev@proton.me>
2023-12-02 18:12:42 -0800

    remove fonts fetching link

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
             href="/img/apple-icon.png"
           />
           {/* Fonts and icons */}
-          <link
+          <!-- <link
             rel="stylesheet"
             type="text/css"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"
@@ -24,7 +24,7 @@ class MyDocument extends Document {
           <link
             href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
             rel="stylesheet"
-          />
+          /> -->
           {/* <!-- Nepcha Analytics (nepcha.com) -->
           <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. --> */}
           <script
